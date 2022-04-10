d4fe0bc08a9b76f84f272f065e5f7f0df4fca02d
Killersmath <maxsuelfilomeno@gmail.com>
2022-04-10 16:08:54 -0300

    Não há necessidade criar um fragment

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -3,17 +3,15 @@ import { BsFillTrashFill } from "react-icons/bs";
 export function List({ listDiv, deleteItem }) {
       
   return (
-    <>
-        {listDiv.map((list,index) => 
-          <div className="list">
-            <input type="checkbox"/>
-            <p>{list} </p>
-            <button className="trash" onClick={() => deleteItem(index)}>
-                <BsFillTrashFill size={17} color="black" />
-            </button>
-          </div>)
-        }
-  </>
+      listDiv.map((list,index) => 
+        <div className="list">
+          <input type="checkbox"/>
+          <p>{list}</p>
+          <button className="trash" onClick={() => deleteItem(index)}>
+              <BsFillTrashFill size={17} color="black" />
+          </button>
+        </div>
+      )
   );
     
 };
